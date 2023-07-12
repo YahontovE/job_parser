2fca543ad069c2207ea85494a9db373a64b9da93
Evgen <yahontov-sport@inbox.ru>
2023-07-12 23:30:00 +0300

    add cod for parse HH_ru

--- src/API.py
+++ src/API.py
@@ -5,47 +5,70 @@ import os
 
 
 class working_with_API(ABC):
+    '''Созадл класс наследующийся от обстактного'''
+
     @abstractmethod
-    def get_data_from_API(self,user_request):
+    def get_data_from_API(self, user_request):
+        '''Обязую классы наследники создавать этот метод'''
         pass
 
 
 class HeadHunterAPI(working_with_API):
+    def __init__(self, user_request):
+        self.user_request = user_request
+
     def get_data_from_API(self):
-        req = requests.get('https://api.hh.ru/vacancies?only_with_salary=true&area=113&per_page=100').json()
-        # for i in req['items']:
-        #    print(i)
-        return req
+        '''Метод получения данных по API'''
+        v_hh = []
+        URL = 'https://api.hh.ru/vacancies'
+        for page in range(1, 11):
+            params = {
+                'per_page': 100,
+                'area': 113,
+                'page': page,
+                'text': self.user_request,
+                'only_with_salary': True
+            }
+            response = requests.get(URL, params=params).json()
+            v_hh.extend(response['items'])
+        return v_hh
+
+    def formated_vacancies(self):
+        '''Этот метод создает из полученных данных спосок с только нужными нам позициями взятыми из вакансии'''
+        hh_vacancies = []
+        for vacanc in self.get_data_from_API():
+            hh_vacancies.append({
+                'title': vacanc['name'],
+                'salary_from': vacanc['salary']['from'],
+                'description': vacanc['snippet']['responsibility'],
+                'url': vacanc['apply_alternate_url']
+            })
+        return hh_vacancies
 
 
 class superjob_ru(working_with_API):
     def __init__(self, user_request):
-    #    self.v = []
         self.user_request = user_request
 
     def get_data_from_API(self):
-        v=[]
+        '''Метод получения данных по API'''
+        v = []
         URL = 'https://api.superjob.ru/2.0/vacancies/'
         SUPERJOB_API_KEY = os.environ.get('SUPERJOB_API_KEY')
         headers = {'X-Api-App-Id': SUPERJOB_API_KEY}
-        for page in range(1,11):
+        for page in range(1, 11):
             params = {
                 'count': 100,
                 'page': page,
                 'keyword': self.user_request,
                 'archive': False
-                # 'payment_from': True
             }
             response = requests.get(URL, headers=headers, params=params).json()
-            # sas=response.content.decode()
-            # js=json.dumps(response,indent=2,ensure_ascii=False)
-            # data = json.loads(js)
-            # print(response['total'])
-            # print(response['total'])
             v.extend(response['objects'])
         return v
 
     def formated_vacancies(self):
+        '''Этот метод создает из полученных данных спосок с только нужными нам позициями взятыми из вакансии'''
         vacancies = []
         for vacanc in self.get_data_from_API():
             vacancies.append({
@@ -58,14 +81,20 @@ class superjob_ru(working_with_API):
 
 
 class Vacancy():
-    def __init__(self,vacancy):
-        self.title =vacancy['title']
-        self.salary_from =vacancy['salary_from']
-        self.description =vacancy['description']
-        self.url =vacancy['url']
+    def __init__(self, vacancy):
+        self.title = vacancy['title']
+        if not isinstance(vacancy['salary_from'], int):
+            self.salary_from = 0
+        else:
+            self.salary_from = vacancy['salary_from']
+        self.description = vacancy['description']
+        self.url = vacancy['url']
 
     def __gt__(self, other):
-        return self.salary_from > other.salary_from
+        '''Метод сравнения экзепляров класса по зарплате '''
+        sc = other if isinstance(other, int) else other.salary_from
+        return self.salary_from > sc
+
     def __str__(self):
         return f'{self.title}\n Зарплата от {self.salary_from}\n{self.url}\nОписание:{self.description}'
 
@@ -74,41 +103,14 @@ class JSONSaver():
     def __init__(self, filename='vacancy.json'):
         self.filename = filename
 
-
     def file_creation(self, vac):
+        '''Записывает в файл отформатированные вакансии'''
         with open(self.filename, 'w', encoding='utf8') as f:
             json.dump(vac, f, indent=2, ensure_ascii=False)
 
     def output_all(self):
+        '''Метод для вывевдения всех полученых вакансий в виде экзепляров класса Vacancy()'''
         with open(self.filename, 'r', encoding='utf8') as file:
             data = json.load(file)
-            vacancy_data=[Vacancy(x) for x in data]
+            vacancy_data = [Vacancy(x) for x in data]
         return vacancy_data
-
-
-class Processing_Vacancies(HeadHunterAPI):
-    def __init__(self):
-        self.name = self.get_data_from_API()['items'][0]['name']
-
-    def get(self):
-        with open('data.json', 'w', encoding='utf8') as file:
-            page = 0
-            while page < 10:
-                url = f'https://api.hh.ru/vacancies?only_with_salary=true&area=113&page={page}&per_page=100'
-                data = requests.get(url).json()
-                file.write(json.dumps(data, ensure_ascii=False))
-                page += 1
-                # for i in self.get_data_from_API()['items']:
-                #    print(i)
-
-# a=HeadHunterAPI()
-# print(a.get_data_from_API())
-# q=Processing_Vacancies()
-# print(q.name)
-# w = superjob_ru()
-# w.get_data_from_API('Python developer')
-# w.get_data_from_API()
-# print(w.get_data_from_API('ьааал'))
-# print(w.formated_vacancies())
-#js=JSONSaver('Врач',[])
-#print(js.output_all())
